e725c5f16339f884c6152ab2fe8e9ec24c72bc90
AmpornSaejaew <amporn.saej@ku.th>
2023-08-09 14:11:19 +0700

    first commit with migration

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="text-3xl bg-auto bg-white bg-contain inline-block pt-5 h-20 px-5 rounded-lg drop-shadow-xl">
+    <h1>
+        Welcome to Laravel
+    </h1>
+    </div>
+@endsection
